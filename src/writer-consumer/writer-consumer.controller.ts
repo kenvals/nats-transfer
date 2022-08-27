11c3b036672be644fd328ab13f436b7ef2052f8b
Kenval <johndoe@example.com>
2022-08-27 15:21:19 +0300

    feat. Add offset

--- src/writer-consumer/writer-consumer.controller.ts
+++ src/writer-consumer/writer-consumer.controller.ts
@@ -1,27 +1,19 @@
-import { Body, Controller, Logger, Post } from '@nestjs/common';
-import {
-  Client,
-  ClientProxy,
-  Ctx,
-  EventPattern,
-  MessagePattern,
-  NatsContext,
-  Payload,
-  Transport,
-} from '@nestjs/microservices';
-import { WriterConsumerService } from './writer-consumer.service';
+import { Body, Controller, Logger, Post } from '@nestjs/common'
+import { EventPattern, Payload } from '@nestjs/microservices'
+import { WriterConsumerService } from './writer-consumer.service'
 
 @Controller('writer-consumer')
 export class WriterConsumerController {
   constructor(private readonly writerConsumerService: WriterConsumerService) {}
-  logger = new Logger('WriterConsumerService');
+  logger = new Logger('WriterConsumerService')
   @EventPattern('binary-data')
   consumer(@Payload() payload: any) {
-    const { _notice, _data } = payload;
+    const { _notice, _data } = payload
     this.logger.debug(
       `<-- Файл пришел в Consumer. Размер: ${_notice.fileSize} bytes`,
-    );
-    this.writerConsumerService.write(_notice, _data);
-    return payload;
+    )
+    this.writerConsumerService.write(_notice, _data)
+
+    return payload
   }
 }
