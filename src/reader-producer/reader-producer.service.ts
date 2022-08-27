9358b8116de62d9cb43a63852cc864efb214a9d7
Kenval <johndoe@example.com>
2022-08-27 19:50:00 +0300

    feat. add works chunk

--- src/reader-producer/reader-producer.service.ts
+++ src/reader-producer/reader-producer.service.ts
@@ -1,10 +1,11 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { Client, ClientProxy, Transport } from '@nestjs/microservices';
+import { Injectable, Logger } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { Client, ClientProxy, Transport } from '@nestjs/microservices'
+import * as fs from 'fs'
 
 @Injectable()
 export class ReaderProducerService {
-  logger = new Logger('ReaderProducerService');
+  logger = new Logger('ReaderProducerService')
   constructor(private readonly configService: ConfigService) {}
   @Client({
     transport: Transport.NATS,
@@ -12,48 +13,57 @@ export class ReaderProducerService {
       url: process.env.NATS_ADDR || 'nats://localhost:4222',
     },
   })
-  client: ClientProxy;
+  client: ClientProxy
   async reader(file: any) {
     const supportedFiles = [
       'image/gif',
       'image/jpeg',
       'image/png',
       'image/webp',
-    ];
+    ]
     // Проверяем поддержку типа файла
     if (!supportedFiles.includes(file.mimetype))
       return this.logger.warn(
         `Файл для загрузки не поддерживается! Тип: ${file.mimetype}`,
-      );
+      )
 
     // Создаем чанки файла
-    this.createChunk(file);
-    return file;
+    this.createChunk(file)
+
+    return file
   }
   createChunk(file: any) {
-    const fileSize = file.size;
-    const fileType = file.mimetype;
-    let currentChunk = 1;
-    const chunkSize = this.configService.get<number>('MAX_PIPE') | 200;
-    const totalChunks = Math.ceil(fileSize / chunkSize / 1024);
-    const notice = { currentChunk, fileType, fileSize };
+    let currentChunk = 1
+
+    const fileSize = file.size
+    const fileType = file.mimetype
+    const chunkSize = Number(this.configService.get<number>('MAX_PIPE'))
+    const totalChunks = Math.ceil(fileSize / chunkSize)
+
+    let notice = {}
 
     while (currentChunk <= totalChunks) {
-      const offset = (currentChunk - 1) * chunkSize;
+      const offset = (currentChunk - 1) * chunkSize
+      const sizeOffset=offset+chunkSize;
       const currentFilePart = Buffer.from(file.buffer).slice(
         offset,
-        offset + chunkSize,
-      );
-      console.log('***');
-      console.log('Current chunk:', currentChunk);
-      console.log('Current chunk data', currentFilePart);
-      this.producer(notice, currentFilePart);
+        sizeOffset,
+      )
+      notice = { currentChunk, totalChunks, fileType, fileSize }
 
-      currentChunk++;
+      console.log('***')
+      console.log('offset:', offset)
+      console.log('Current chunk:', currentChunk)
+      console.log('Current chunk data', currentFilePart)
+
+      this.producer(notice, currentFilePart)
+
+
+      currentChunk++
     }
   }
 
   producer(_notice, _data) {
-    this.client.emit('binary-data', { _notice, _data });
+    this.client.emit('binary-data', { _notice, _data })
   }
 }
--- src/writer-consumer/writer-consumer.service.ts
+++ src/writer-consumer/writer-consumer.service.ts
@@ -1,19 +1,17 @@
-import { Injectable } from '@nestjs/common';
-import * as fs from 'fs';
-import { v4 as uuid } from 'uuid';
+import { Injectable } from '@nestjs/common'
+import * as fs from 'fs'
+import { v4 as uuid } from 'uuid'
 
 @Injectable()
 export class WriterConsumerService {
-  async write(notice, data) {
-    console.log(data);
-    const buffer = Buffer.from(data.data);
-    console.log(buffer);
-    // console.log(data.data);
-    // const id: string = uuid();
-    // fs.writeFile(
-    //   '/Users/kenval/Desktop/R-vision/data/image.txt',
-    //   Buffer.from(data.data),
-    //   'binary',
-    // );
+  write(notice, chunks) {
+    let { totalChunks, currentChunk } = notice
+    const { data } = chunks
+
+    fs.writeFile(
+      '/Users/kenval/Desktop/temp/image.png',
+      Buffer.from(data),
+      () => console.log('saved chunks!'),
+    )
   }
 }
